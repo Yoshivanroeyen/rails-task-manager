392a68ec41afbc6adf3db5ac81ba241a09d7619a
yoshi van roeyen <yoshivanroeyen@gmail.com>
2017-10-31 15:07:05 +0100

    Updated task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-
+  before_action :set_task, only: [:show, :update, :edit, :destroy]
   def index
 
     @tasks = Task.all
@@ -18,23 +18,20 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
+
   end
 
   def edit
 
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to tasks_path
-
   end
 
   def destroy
-    @task = Task.find(params[:id])
+
     @task.destroy
     redirect_to tasks_path
   end
@@ -43,6 +40,10 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:content, :date, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 
 end
